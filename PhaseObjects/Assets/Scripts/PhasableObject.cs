d5d4ccbd500a7b58359e713f3a908dbc0e62c683
Niall Nulty <nnulty97@gmail.com>
2020-10-29 00:36:21 +0000

    PhasableObject & PhaseController set up

---
+++ PhaseObjects/Assets/Scripts/PhasableObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhasableObject : MonoBehaviour
+{
+    //Enum to determine if object is phased or not
+    public enum PhaseType
+    {
+        Unphased,
+        Phased
+    }
+
+    public PhaseType Type;
+
+    //Materials
+    [SerializeField] private Material unphasedMaterial;
+    [SerializeField] private Material phasedMaterial;
+
+    void Start()
+    {
+        //Checks type on start so type can be set before in inspector
+        CheckType();
+    }
+
+    //Sets object values for type
+    public void CheckType()
+    {
+        //Unphased, solid material and box collider enabled
+        if (Type == PhaseType.Unphased)
+        {
+            this.transform.GetComponent<BoxCollider>().enabled = true;
+            this.transform.GetComponent<MeshRenderer>().material = unphasedMaterial;
+        }
+
+        //Phased, transparent material and box collider disabled
+        else
+        {
+            this.transform.GetComponent<BoxCollider>().enabled = false;
+            this.transform.GetComponent<MeshRenderer>().material = phasedMaterial;
+        }
+    }
+
+    //for changing phase type
+    public void ChangeType()
+    {
+        if (Type == PhaseType.Unphased)
+        {
+            Type = PhaseType.Phased;
+        }
+
+        else
+        {
+            Type = PhaseType.Unphased;
+        }
+    }
+}
---
+++ PhaseObjects/Assets/Scripts/PhaseController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseController : MonoBehaviour
+{
+    //list of all phasable objects
+    [SerializeField] private List<GameObject> phasableObjects = new List<GameObject>();
+
+    //change phasable objects phase type
+    public void Phase()
+    {
+        foreach (GameObject phasableObject in phasableObjects)
+        {
+            phasableObject.GetComponent<PhasableObject>().ChangeType();
+            phasableObject.GetComponent<PhasableObject>().CheckType();
+        }
+    } 
+}
